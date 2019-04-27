3e16b7825122ed8967e93b60324a09ec422870a3
gyujvari <46863344+gyujvari@users.noreply.github.com>
2019-04-27 17:02:50 +0300

    Add files via upload

---
+++ chillzone/scripts/AdScript.cs
@@ -0,0 +1,39 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        showBannerAd();
+
+    }
+
+    private void showBannerAd()
+    {
+        string adID = "ca-app-pub-4325721457220275/4269591340";
+
+        //***For Testing in the Device***
+        AdRequest request = new AdRequest.Builder()
+       .AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+       .AddTestDevice("3D0E903C2C2E98735BCCE6022614913B")  // My test device.
+       .Build();
+
+        //***For Production When Submit App***
+        //AdRequest request = new AdRequest.Builder().Build();
+
+        BannerView bannerAd = new BannerView(adID, AdSize.SmartBanner, AdPosition.Top);
+        bannerAd.LoadAd(request);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ chillzone/scripts/loadingbar.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingbar : MonoBehaviour {
+
+    public GameObject loadingbaar;
+    public GameObject textindicator;
+    public GameObject textloading;
+    public AudioSource viz;
+    public GameObject exitbutton;
+    public GameObject stopbutton;
+
+
+
+    public float currentamount;
+
+   
+    public float speed;
+
+    
+
+    public void start()
+    {
+        
+        loadingbaar.SetActive(true);
+        viz.Play();
+
+
+
+
+    }
+    
+
+        
+
+
+    
+
+
+
+    // Update is called once per frame
+    public void Update ()
+    
+    {
+            
+            if (currentamount < 100)
+            {
+                
+                currentamount += speed * Time.deltaTime;
+                textindicator.GetComponent<Text>().text = ((int)currentamount).ToString() + "";
+                textloading.gameObject.SetActive(true);
+               
+
+
+        }
+            else
+            {
+                
+                textloading.gameObject.SetActive(false);
+                textindicator.GetComponent<Text>().text = "vege";
+                
+
+            }
+            loadingbaar.GetComponent<Image>().fillAmount = currentamount / 100;
+        
+
+    }
+
+    public void QuitGame()
+    {
+        loadingbaar.SetActive(false);
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+    public void stop()
+    {
+        stopbutton.SetActive(false);
+        loadingbaar.SetActive(false);
+        exitbutton.SetActive(true);
+        viz.Stop();
+
+
+    }
+}
