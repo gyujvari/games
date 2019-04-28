5e2027faadc39f63ad4a8fee73cb1f279e4c15c0
Ujvari Gyorgy <46863344+gyujvari@users.noreply.github.com>
2019-04-28 15:22:30 +0300

    Add files via upload
    rotation movement script for buttons or menus

---
+++ RotationMovementScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationMovementScript : MonoBehaviour {
+
+	public float RotationSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 vec = new Vector3 (0, 0, RotationSpeed);  
+		this.transform.Rotate (vec);
+	
+	}
+}
